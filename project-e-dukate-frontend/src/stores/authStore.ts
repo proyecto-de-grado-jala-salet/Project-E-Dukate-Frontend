02fa443b53a27656b40ee1b1b035d165fe71b035
salet-gutierrez-jalafund <salet.gutierrez@fundacion-jala.org>
2025-05-10 23:40:43 -0400

    chore: delete unnecessary comment

--- project-e-dukate-frontend/src/stores/authStore.ts
+++ project-e-dukate-frontend/src/stores/authStore.ts
@@ -4,7 +4,7 @@ import { jwtDecode } from 'jwt-decode';
 
 interface DecodedToken {
   sub: string;
-  [key: string]: string | number; // Permitir claves dinámicas
+  [key: string]: string | number;
   'http://schemas.microsoft.com/ws/2008/06/identity/claims/role': string;
   'http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name': string;
   exp: number;
