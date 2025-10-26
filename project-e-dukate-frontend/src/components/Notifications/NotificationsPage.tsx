df5455670d9c910f9617f125d25cb10a3e5017c6
salet-gutierrez-jalafund <salet.gutierrez@fundacion-jala.org>
2025-10-26 19:28:34 -0400

    fix: add unoptimized in the Image comprobante

--- project-e-dukate-frontend/src/components/Notifications/NotificationsPage.tsx
+++ project-e-dukate-frontend/src/components/Notifications/NotificationsPage.tsx
@@ -421,6 +421,7 @@ const NotificationsPage: React.FC = () => {
                     alt="Comprobante de pago"
                     width={600}
                     height={400}
+                    unoptimized={true}
                     style={{
                       maxWidth: "100%",
                       height: "auto",
